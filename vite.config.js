efaedf58b3abbfa93bfcb298bf78958fdf4ae380
FranciscoRV <nosoyunmarinero@gmail.com>
2026-05-17 23:18:03 -0600

    GH pages deployed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/catsino/', // 🌟 Agregado para que GitHub Pages mapee bien los assets del Catsino
   server: {
     port: 3000,
     open: true
