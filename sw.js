664b39cf477bed4f95e04422a83f8d44bb2e71c1
Francisco Romero <nosoyunmarinero@gmail.com>
2026-05-19 17:56:28 -0600

    Monetag file added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11026430
+    "zoneId": 11030438
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
